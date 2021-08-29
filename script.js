88e3224775fc2336397dbce4b4511460327b7afa
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-29 16:29:21 -0300

    add in game funcionando

--- script.js
+++ script.js
@@ -44,34 +44,37 @@ const playBtn = document.querySelector('.play');
 playBtn.addEventListener('click', toStart);
 
 const stop = () => {
-  const bigLi = document.querySelectorAll('.big-li');
-  const inputPlay = document.querySelectorAll('.input-play');
-  for (let i = 0; i < inputPlay.length; i += 1) {
-    const smallLi = document.createElement('li');
-    const select = document.createElement('select');
-    const option0 = document.createElement('option');
-    const option5 = document.createElement('option');
-    const option10 = document.createElement('option');
-    const div = document.createElement('div');
-    div.className = 'put-in-line';
-    option0.innerHTML = '0';
-    option5.innerHTML = '5';
-    option10.innerHTML = '10';
-    select.className = 'select-play';
-    smallLi.className = 'small-li';
-    smallLi.innerHTML = inputPlay[i].value;
-    if (smallLi.innerHTML === "") {
-      select.appendChild(option0);
-    } else {
-      select.appendChild(option0);
-      select.appendChild(option5);
-      select.appendChild(option10);
+  const selectPoints = document.querySelectorAll('.select-play');
+  if (selectPoints.length === 0) {
+    const bigLi = document.querySelectorAll('.big-li');
+    const inputPlay = document.querySelectorAll('.input-play');
+    for (let i = 0; i < inputPlay.length; i += 1) {
+      const smallLi = document.createElement('li');
+      const select = document.createElement('select');
+      const option0 = document.createElement('option');
+      const option5 = document.createElement('option');
+      const option10 = document.createElement('option');
+      const div = document.createElement('div');
+      div.className = 'put-in-line';
+      option0.innerHTML = '0';
+      option5.innerHTML = '5';
+      option10.innerHTML = '10';
+      select.className = 'select-play';
+      smallLi.className = 'small-li';
+      smallLi.innerHTML = inputPlay[i].value;
+      if (smallLi.innerHTML === "") {
+        select.appendChild(option0);
+      } else {
+        select.appendChild(option0);
+        select.appendChild(option5);
+        select.appendChild(option10);
+      }
+      smallLi.appendChild(select);
+      div.appendChild(smallLi);
+      div.appendChild(select);
+      bigLi[i].appendChild(div);
+      inputPlay[i].value = '';
     }
-    smallLi.appendChild(select);
-    div.appendChild(smallLi);
-    div.appendChild(select);
-    bigLi[i].appendChild(div);
-    inputPlay[i].value = '';
   }
 }
 
@@ -89,10 +92,10 @@ const sum = () => {
       result = parseInt(result) + parseInt(valor);
       const pPoint = document.createElement('p');
       pPoint.className = 'point-in-p'
-      if (valor === 0) {
+      pPoint.innerHTML = valor;
+      if (pPoint.innerHTML === "0") {
         pPoint.classList.add('zero');
       }
-      pPoint.innerHTML = valor;
       selectPoints[i].parentNode.appendChild(pPoint);
       selectPoints[i].parentNode.removeChild(selectPoints[i]);
       turnPoint = (turnPoint + parseInt(valor));
@@ -108,3 +111,32 @@ const sum = () => {
 
 const sumBtn = document.querySelector('.sum');
 sumBtn.addEventListener('click', sum);
+
+const addTheme = () => {
+  const addInGame = document.querySelector('#ad-input');
+  if (addInGame.value.length !== 0) {
+    const ulDiv = document.createElement('div');
+    const bigLi = document.createElement('li');
+    const inputPlay = document.createElement('input');
+    inputPlay.className = 'input-play';
+    bigLi.className = 'big-li';
+    bigLi.innerText = addInGame.value;
+    bigLi.appendChild(inputPlay);
+    ulDiv.appendChild(bigLi);
+    ulTemas.appendChild(ulDiv);
+    addInGame.value = '';
+    const turnNum = document.querySelector('.turn-points').children.length;
+    for (let i = 0; i < turnNum; i += 1) {
+      const p = document.createElement('p');
+      p.className = 'point-in-p zero';
+      p.innerHTML = 0;
+      const div = document.createElement('div');
+      div.className = 'put-in-line';
+      div.appendChild(p);
+      bigLi.appendChild(div);
+    }
+  }
+}
+
+const btnAd = document.querySelector('.ad-button');
+btnAd.addEventListener('click', addTheme);
