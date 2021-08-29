587de03c360266f4747cce9e09a272b318172033
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-29 14:29:48 -0300

    script stop

--- script.js
+++ script.js
@@ -16,14 +16,11 @@ const createThemes = () => {
   for (let i = 0; i < listTheme.children.length; i += 1) {
     const ulDiv = document.createElement('div');
     const bigLi = document.createElement('li');
-    const smallLi = document.createElement('li');
     const inputPlay = document.createElement('input');
     inputPlay.className = 'input-play';
     bigLi.className = 'big-li';
-    smallLi.className = 'small-li';
     bigLi.innerText = listTheme.children[i].innerText;
-    smallLi.appendChild(inputPlay);
-    bigLi.appendChild(smallLi);
+    bigLi.appendChild(inputPlay);
     ulDiv.appendChild(bigLi);
     ulTemas.appendChild(ulDiv);
   }
@@ -44,3 +41,32 @@ const toStart = () => {
 const playBtn = document.querySelector('.play');
 playBtn.addEventListener('click', toStart);
 
+const stop = () => {
+  const bigLi = document.querySelectorAll('.big-li');
+  const inputPlay = document.querySelectorAll('.input-play');
+  for (let i = 0; i < inputPlay.length; i += 1) {
+    const smallLi = document.createElement('li');
+    const select = document.createElement('select');
+    const option0 = document.createElement('option');
+    const option5 = document.createElement('option');
+    const option10 = document.createElement('option');
+    const div = document.createElement('div');
+    div.className = 'put-in-line';
+    option0.innerHTML = '0';
+    option5.innerHTML = '5';
+    option10.innerHTML = '10';
+    select.appendChild(option0);
+    select.appendChild(option5);
+    select.appendChild(option10);
+    select.className = 'select-play';
+    smallLi.className = 'small-li';
+    smallLi.innerHTML = inputPlay[i].value;
+    smallLi.appendChild(select);
+    div.appendChild(smallLi);
+    div.appendChild(select);
+    bigLi[i].appendChild(div);
+  }
+}
+
+const stopBtn = document.querySelector('.stop');
+stopBtn.addEventListener('click', stop);
